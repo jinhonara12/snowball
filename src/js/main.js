a1c7ddeb49ac6423ed027580a5167f6c926b8961
jinho <jinhonara12@naver.com>
2024-02-09 17:08:23 +0900

    경로 변경

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,4 @@
-// fetch("/index.js")
-//     .then((response, reject) => {
-//         return response
-//     })
-//     .then(data => {
-//         console.log(data)
-//     })
-
-// let result = function (data) {
-//     let test = document.querySelector('#test');
-//     for (let i = 0; i < data.length; i++) {
-//         let p = document.createElement('p');
-//         p.textContent = JSON.stringify(data[i])
-//         test.appendChild(p)
-//     }
-// }
-
-fetch("/list.json")
+fetch("./list.json")
     .then((response, reject) => {
         return response.json()
     })
